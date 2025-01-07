4cf160d5504d454ca84938afe25d3879486741a1
Thomas Dupoiron <tdupoiron@github.com>
2025-01-07 10:51:52 +0000

    Update repository credentials in ECS task definition and modify payload parsing in index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.post("/", express.json(), async (req, res) => {
   console.log("User:", user.data.login);
 
   // Parse the request payload and log it.
-  const payload = req.body.replace(/\n|\r/g, "");
+  const payload = req.body;
   console.log("Payload:", payload);
 
   // Write client ip
